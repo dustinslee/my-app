71fe4a13853cee86fdfb6d8e29c1bfb6e10f07d3
Dustin Lee <dustinsl094@gmail.com>
2023-02-01 12:34:55 -0500

    Setting up and implementing react router

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <h1 id="mainHeader">React Router</h1>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index path="/" element={<Home />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="*" element={<NoMatch />}/>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import './NavBar.css';
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <ul id='nav-ul'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
